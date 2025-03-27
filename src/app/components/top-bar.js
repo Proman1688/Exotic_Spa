5866110446da82e892b302fc6908f9412084be97
JADODEVS <jadodevs@gmail.com>
2025-03-26 20:19:55 -0500

    top-bar con resposive

--- src/app/components/top-bar.js
+++ src/app/components/top-bar.js
@@ -7,25 +7,25 @@ export default function TopBar() {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
     </Head>
     
-    <nav className="flex justify-between p-2 px-16 text-xs bg-amber-50 text-black dark:bg-[#2a24242d] dark:text-white">
-      <p className="flex items-center">Welcome to our online store</p>
+    <nav className="flex justify-between p-2 px-16 text-xs bg-amber-50 text-black dark:bg-[#2a24242d] dark:text-white max-sm:px-5">
+      <p className="flex items-center max-sm:hidden">Welcome to our online store</p>
 
-      <div className="flex items-center space-x-5">
-        <select>
+      <div className="flex items-center space-x-5 max-sm:space-x-2 max-sm:w-full max-sm:justify-between">
+        <select className="max-sm:text-[10px]">
           <option className="dark:bg-white dark:text-black">English</option>
           <option className="dark:bg-white dark:text-black">Spanish</option>
           <option className="dark:bg-white dark:text-black">French</option>
         </select>
-        <a className="border-l-2 px-5" href="#">Login or Register</a>
+        <a className="border-l-2 px-5 max-sm:text-[10px] max-sm:px-1 text-center max-sm:w-36" href="#">Login or Register</a>
         <div className="flex space-x-4">
           <a href="https://github.com/jadodevs">
-            <img className="invert dark:invert-0" src="/github.svg" alt="GitHub" width="30" height="30" />
+            <img className="invert dark:invert-0  " src="/github.svg" alt="GitHub" width="30" height="30" />
           </a>
           <a href="https://instagram.com/mi_usuario">
-            <img className="invert dark:invert-0" src="/instagram.svg" alt="Instagram" width="30" height="30" />
+            <img className="invert dark:invert-0  " src="/instagram.svg" alt="Instagram" width="30" height="30" />
           </a>
           <a href="https://linkedin.com/in/mi_usuario">
-            <img className="invert dark:invert-0" src="/linkedin.svg" alt="LinkedIn" width="30" height="30" />
+            <img className="invert dark:invert-0  " src="/linkedin.svg" alt="LinkedIn" width="30" height="30" />
           </a>
         </div>
       </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

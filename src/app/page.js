e7a7171c2d05732599350ebd3097024acd76f11a
seqenenra08 <juandavidalvarez08@gmail.com>
2025-03-28 22:01:27 -0500

    la segunda seccion promocional

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import { PromoSection } from "./components/main-page/PromoSection";
+import { PromoSecondSection } from "./components/main-page/PromoSecondSection";
+
 export default function Home() {
   return (
   <>
   <div className="flex items-center justify-center">
     <PromoSection />
+  </div>
+  <div className="flex items-center justify-center">
+    <PromoSecondSection />
   </div>
   </>
   );
